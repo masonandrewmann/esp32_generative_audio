d0c742deb2fe814f65e3c661a3f4d44aa7c1a40a
Mason Mann <masonandrewmann@gmail.com>
2020-11-14 16:58:36 -0500

    prototype of wavetable synth with sine table loaded

--- audio_engine.ino
+++ audio_engine.ino
@@ -1,9 +1,24 @@
+int SineValues[256];  
+
 void setup() {
-  // put your setup code here, to run once:
+  generateSineWavetable();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void generateSineWavetable(){
+  float ConversionFactor = (2 * PI) / 256;
+  float RadAngle;
+
+  for(int MyAngle=0;MyAngle<256;MyAngle+=) {
+  RadAngle=MyAngle*ConversionFactor;               // 8 bit angle converted to radians
+  SineValues[MyAngle]=(sin(RadAngle)*127)+128;  // get the sine of this angle and 'shift' up so
+                                            // there are no negative values in the data
+                                            // as the DAC does not understand them and would
+                                            // convert to positive values.
+  }
 }
