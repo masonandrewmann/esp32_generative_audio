04dc40be7b1b0744ae19c3201f8b4e53d8fae472
Mason Mann <masonandrewmann@gmail.com>
2020-11-22 18:03:48 -0500

    polyphony working but frequency is real drifty, also there are some pops where the output is randomly zero for some reason. well not random because it happens at the same point every cycle

--- audio_engine.ino
+++ audio_engine.ino
@@ -5,12 +5,12 @@ int SineValues[tableSize];       // an array to store our values for sine
 int sineCounter = 0;
 
 float outputVal = 0;
+int usTime = 0;
 
 class SinOsc {
   //wavetable oscillator with linear interpolation
   //1024 samples played at sample rate of 8000Hz
   private:
-    int usTime;
     float pointerInc;
     float pointerVal = 0;
     
@@ -34,28 +34,25 @@ class SinOsc {
       float rem = (pointerVal - floor(pointerVal));
       outVal = lower + (rem) * (upper - lower);
       }
-      dacWrite(26, SineValues[(int)pointerVal] * mul);
+//      dacWrite(26, outVal * mul);
+      outputVal = outputVal + mul * (outVal - 128);
+      
       pointerVal += pointerInc;
       if(pointerVal > tableSize) pointerVal -= tableSize;
     }
 
     void cycle(){
-//      Serial.println(micros());
-      if ( micros() > usTime){
         pointerInc = 1024 * (freq / 8000);
         calc();
-        usTime = micros() + 125;
-      }
     }
 };
 
 //make a test oscillator
-  SinOsc myOsc(440, 1);
-  SinOsc myOsc2(
+  SinOsc myOsc(220, 0.3);
+  SinOsc myOsc2(277.18, 0.2);
   
 void setup()
 {
-//  Serial.begin(9600);
   // calculate sine values
   float RadAngle;                           // Angle in Radians
   for(int MyAngle=0;MyAngle<tableSize;MyAngle++) {
@@ -66,33 +63,50 @@ void setup()
  
 void loop()
 {
+  if ( micros() > usTime){
+    //reset output value
+    outputVal = 0;
+  
+    //cycle oscillators
+    myOsc.cycle();
+    myOsc2.cycle();
+  
+    //write output to pin
+    outputVal += 128;
+    if (outputVal < 0) outputVal = 0;
+    else if (outputVal > 255) outputVal = 255;
+    dacWrite(26, outputVal);
 
-  myOsc.cycle();
+    
+    usTime = micros() + 125;
+  }
+
+
+  //sequence frequencies
   if ((millis() % 2000) > 1000) {
     myOsc.freq = 220;
   } else {
     myOsc.freq = 293.66;
   }
 
-//  if ((millis() % 2000) < 250) {
-//    myOsc2.freq = 440;
-//  } else if ((millis() % 2000) < 500){
-//    myOsc2.freq = 554.37;
-//  } else if ((millis() % 2000) < 750){
-//    myOsc2.freq = 659.25;
-//  } else if ((millis() % 2000) < 1000){
-//    myOsc2.freq = 554.37;
-//  } else if ((millis() % 2000) < 1250){
-//    myOsc2.freq = 440;
-//  } else if ((millis() % 2000) < 1500){
-//    myOsc2.freq = 587.33;
-//  } else if ((millis() % 2000) < 1750){
-//    myOsc2.freq = 698.46;
-//  } else {
-//    myOsc2.freq = 587.33;
-//  }
+  if ((millis() % 2000) < 250) {
+    myOsc2.freq = 440;
+  } else if ((millis() % 2000) < 500){
+    myOsc2.freq = 554.37;
+  } else if ((millis() % 2000) < 750){
+    myOsc2.freq = 659.25;
+  } else if ((millis() % 2000) < 1000){
+    myOsc2.freq = 554.37;
+  } else if ((millis() % 2000) < 1250){
+    myOsc2.freq = 440;
+  } else if ((millis() % 2000) < 1500){
+    myOsc2.freq = 587.33;
+  } else if ((millis() % 2000) < 1750){
+    myOsc2.freq = 739.99;
+  } else {
+    myOsc2.freq = 587.33;
+  }
   
-//  Serial.println(myOsc.freq);
 }
 
 void playSine(float freq, float len){
