e054d7ea11e2a5b44a8ca969e2b5181202941ac9
Mason Mann <masonandrewmann@gmail.com>
2020-11-25 18:08:17 -0500

    added the code to h and cpp, not working yet thooo

--- esp_gen_audio.cpp
+++ esp_gen_audio.cpp
@@ -1,1 +1,86 @@
+#include "esp_gen_audio.h"
+#include <Arduino.h>
 
+void SinOsc::cycle(void){
+        pointerInc = TABLESIZE * (freq / SAMPLEHZ);
+        if ((pointerVal - floor(pointerVal)) == 0){                //if pointerVal lands on an integer index use it
+          outVal = SineValues[(int)pointerVal];
+        } else {                               //if pointerVal lands between integer indexes, linearly interpolate the between adjacent samples
+          int lower = SineValues[(int)floor(pointerVal)];                        //sample on lower side
+          int upper = SineValues[(int)ceil(pointerVal)];                        //sample on higher side
+          float rem = (pointerVal - (int)floor(pointerVal));
+          outVal = (rem - 0)*(upper - lower) / (1 - 0) + lower;
+          if (outVal > 300) outVal = lower;
+        }
+        outputVal = outputVal + mul * envGain * (outVal - 128);
+        
+        pointerVal += pointerInc;
+        if(pointerVal > TABLESIZE) pointerVal -= TABLESIZE;
+}
+
+void EnvGen::cycle(void){
+  //        Serial.print("Gain: ");
+//    Serial.println(currVol);
+    if (trig){
+      active = true;
+      trig = false;
+      state = 0;
+      startTime = millis();
+      nextTime = startTime + *(times + state);
+    }
+    //determine current state
+    if ( active && millis() > nextTime){
+      //go to next state
+      state++;
+      startTime = millis();
+      nextTime = startTime + *(times + state);
+      if ( state >= numStates - 1){
+        active = false;
+        currVol = *(levels + state);
+      }
+    }
+    if (active){
+      float startLevel = *(levels + state);
+      float nextLevel = *(levels + state + 1);
+      
+//    Serial.print("State: ");
+//    Serial.println(state);
+//    Serial.print("active?:");
+//    Serial.println(active);
+//    Serial.print("Gain: ");
+//    Serial.println(currVol);
+//    Serial.print("startTime: ");
+//    Serial.println(startTime);
+//    Serial.print("nextTime: ");
+//    Serial.println(nextTime);
+//    Serial.print("startLevel: ");
+//    Serial.println(startLevel);
+//    Serial.print("nextLevel: ");
+//    Serial.println(nextLevel);
+//    Serial.print("currTime: ");
+//    Serial.println(millis());
+//    Serial.println();
+      //calculate output gain
+      currVol = (millis() - startTime) * (nextLevel - startLevel) / (nextTime - startTime) + startLevel;
+      if (currVol > 1){
+        currVol = 1;
+      } else if (currVol < 0){
+        currVol = 0;
+      }
+    }
+    destOsc->envGain = currVol;
+}
+
+void Sequencer::cycle(void){
+  //      Serial.print("freq: ");
+  //      Serial.println(currVal);
+      int msTime = millis();
+      if (msTime > goalTime){
+        valNumber = (valNumber + 1) % valsLen;
+        currVal = *(vals + valNumber);
+        goalTime += dur;
+  //        trig = true;
+          destEnv->trig = true;
+      }
+      destEnv->destOsc->freq = currVal;
+}
--- esp_gen_audio.h
+++ esp_gen_audio.h
@@ -1,8 +1,103 @@
-#ifndef esp_gen_audio
-#define esp_gen_audio
+#define TABLESIZE 1024
+#define SAMPLEHZ 44100
+
+#include <Arduino.h>
+
+#ifndef esp_gen_audio_h_
+#define esp_gen_audio_h_
+
 
 
 /************************************
  * For generative audio on the ESP-32
  *               :~)
  */
+
+
+ 
+class SinOsc {
+  //wavetable oscillator with linear interpolation
+  //1024 samples played at sample rate of 8000Hz
+  private:
+    float pointerInc;
+    float pointerVal = 0;
+    
+  public:
+    float freq;
+    float mul;
+    int outVal;
+    float envGain;
+    
+    SinOsc(float freq, float phase, float mul){
+      this->freq = freq;
+      this->mul = mul;
+      pointerInc = TABLESIZE * (freq / SAMPLEHZ);
+      pointerVal = map(phase, 0, TWO_PI, 0, TABLESIZE - 1);
+      envGain = 1;
+    }
+    
+    void cycle(){
+    }
+};
+
+class EnvGen {
+  // linear interpolating envelope generator of any length
+  // series of volume levels (between 0 and 1), length  of time between them (in milliseconds), and number of levels
+  private:
+    int numStates;
+    int state;
+    float startTime;
+    float nextTime;
+  public:
+    float *levels;
+    float *times;
+    float currVol;
+    boolean active;
+    boolean trig;
+    SinOsc* destOsc;
+  EnvGen( float *levels, float *times, int numStates, SinOsc* destOsc){
+    this->levels = levels;
+    this->times = times;
+    this->numStates = numStates;
+    this->destOsc = destOsc;
+    state = 0;
+    startTime = millis();
+    nextTime = startTime + *(times + state);
+    currVol = 0;
+    active = false;
+    trig = false;
+  }
+
+  void cycle(){
+  }
+};
+
+class Sequencer {
+  private:
+    int valNumber;
+    int valsLen;
+    int goalTime;
+    EnvGen* destEnv;
+  public:
+    float *vals;
+    float dur;
+    float currVal;
+    boolean trig;
+
+    Sequencer(float *vals, float dur, int valsLen, EnvGen* destEnv){
+      this->vals = vals;
+      this->dur = dur;
+      this->valsLen = valsLen;
+      this->destEnv = destEnv;
+      currVal = *vals;
+      valNumber = 0;
+      goalTime = 0;
+//      trig = true;
+        destEnv->trig = true;
+    }
+
+    void cycle(){
+    }
+};
+
+#endif
